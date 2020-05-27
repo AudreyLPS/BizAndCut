a3b029536a2f8751c31980b83548fa52dc51a9ed
oulima <mimandiaye5672@gmail.com>
2020-05-27 15:32:37 +0200

    modif proposition

--- src/Form/PropositionType.php
+++ src/Form/PropositionType.php
@@ -5,31 +5,50 @@ namespace App\Form;
 use App\Entity\Devis;
 use App\Entity\Coiffeurs;
 use App\Entity\Propositions;
+use App\Repository\DevisRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class PropositionType extends AbstractType
 {
+    public function __construct(Security $security, DevisRepository $devisRepository)
+    {
+        $this->security = $security;
+        $this->devis = $devisRepository->find(1);
+    }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $coiffeur = $this->security->getUser();
         $builder
+            ->add('coiffeur', HiddenType::class, [
+                'empty_data'=> $coiffeur,
+                'data'=>""
+            ])
             ->add('devis', EntityType::class, [
                 'class'=>Devis::class,
                 'choice_label'=>'id',
                 'placeholder'=>''
             ])
-            ->add('coiffeur', EntityType::class, [
-                'class'=>Coiffeurs::class,
-                'choice_label'=>'id',
-                'placeholder'=>''
+           
+            ->add('validationBC', HiddenType::class, [
+                'empty_data'=> 0,
             ])
-            ->add('validationBC')
-            ->add('dateProposition')
-            ->add('validationentreprise')
+            ->add('dateProposition', HiddenType::class, [
+                'empty_data'=> new \DateTime(), 'data' =>'',
+            ])
+
+        
+            ->add('validationentreprise', HiddenType::class, [
+                'empty_data'=> 0,
+            ])
             ->add('montant', NumberType::class, [
                 'constraints' => [
                     new NotBlank([
